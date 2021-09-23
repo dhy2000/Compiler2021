0844949de0d88bcdde07836d5e76b2e91da7b692
18375354 <18375354@buaa.edu.cn>
2021-09-23 21:46:10 +0800

    fix bug on skipping comment

--- src/compiler/process/Lexer.java
+++ src/compiler/process/Lexer.java
@@ -85,8 +85,11 @@ public class Lexer {
 
     private static void skipBlockComment(SourceBuffer source) {
         if ("/*".equals(source.followingSeq(2))) {
-            while (!"*/".equals(source.followingSeq(2))) {
+            while (!source.reachedEndOfFile() && !"*/".equals(source.followingSeq(2))) {
                 source.forward(1);
+            }
+            if ("*/".equals(source.followingSeq(2))) {
+                source.forward(2);
             }
         }
     }
