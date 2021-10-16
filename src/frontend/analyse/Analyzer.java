c71436f76448107bb11642337b51e667074c5bc5
18375354 <18375354@buaa.edu.cn>
2021-10-17 04:19:03 +0800

    todo analyzer

---
+++ src/frontend/analyse/Analyzer.java
@@ -0,0 +1,13 @@
+package frontend.analyse;
+
+/**
+ * 语义分析器：遍历语法树，维护符号表，进行错误处理，生成中间代码（略）
+ * 和语法分析类似的类递归下降结构
+ */
+public class Analyzer {
+    // TODO: analyseBlock
+    // TODO: analyseStmt
+    // TODO: analyseExp
+    // TODO: analyseFunc
+    // TODO: analyseDecl
+}
