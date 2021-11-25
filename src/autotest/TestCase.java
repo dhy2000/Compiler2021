09e086030cb56f868b624db410f0633d1b234a0d
18375354 <18375354@buaa.edu.cn>
2021-11-26 03:01:10 +0800

    store work

---
+++ src/autotest/TestCase.java
@@ -0,0 +1,15 @@
+package autotest;
+
+/**
+ * 以对象方式存储自动测试点类
+ */
+public class TestCase {
+    /*
+        name - String
+        compressed - boolean
+        testfile - byte[] or String or from-file
+        input - byte[] or String or from-file
+
+     */
+
+}
---
+++ src/autotest/TestUtil.java
@@ -0,0 +1,4 @@
+package autotest;
+
+public class TestUtil {
+}
