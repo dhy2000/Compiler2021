d881b8bc695925f9d2dd1aab4ed7c20884963062
18375354 <18375354@buaa.edu.cn>
2021-11-26 00:18:16 +0800

    adjust

--- src/Compiler.java
+++ src/Compiler.java
@@ -53,14 +53,14 @@ public class Compiler {
 
     public static void main(String[] args) {
         // load arguments
+        String[] argsDefault = new String[]{"-s", "testfile.txt", "-O", "mips.txt"};
+        Config config = Config.fromArgs(args.length > 0 ? args : argsDefault);
+        if (Objects.isNull(config)) {
+            System.err.println("Compiler failed launch due to previous errors.");
+            System.out.println(Config.usage());
+            System.exit(0);
+        }
         try {
-            String[] argsDefault = new String[]{"-s", "testfile.txt", "-O", "mips.txt"};
-            Config config = Config.fromArgs(args.length > 0 ? args : argsDefault);
-            if (Objects.isNull(config)) {
-                System.err.println("Compiler failed launch due to previous errors.");
-                System.out.println(Config.usage());
-                System.exit(0);
-            }
             Compiler compiler = new Compiler(config);
             compiler.run();
         } catch (Exception e) {
