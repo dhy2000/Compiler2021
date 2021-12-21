23a66144ed049f496c9bd452eb58c4e004d08b7e
18375354 <18375354@buaa.edu.cn>
2021-12-22 00:21:50 +0800

    add nor

---
+++ src/backend/instruction/NotOr.java
@@ -0,0 +1,35 @@
+package backend.instruction;
+
+import backend.hardware.RegisterFile;
+
+public class NotOr extends MipsInstruction {
+    private final int regSrc1;
+    private final int regSrc2;
+    private final int regDst;
+
+    public NotOr(int regSrc1, int regSrc2, int regDst) {
+        this.regSrc1 = regSrc1;
+        this.regSrc2 = regSrc2;
+        this.regDst = regDst;
+    }
+
+    public int getRegSrc1() {
+        return regSrc1;
+    }
+
+    public int getRegSrc2() {
+        return regSrc2;
+    }
+
+    public int getRegDst() {
+        return regDst;
+    }
+
+    @Override
+    public String instrToString() {
+        return String.format("nor $%s, $%s, $%s",
+                RegisterFile.getRegisterName(regDst),
+                RegisterFile.getRegisterName(regSrc1),
+                RegisterFile.getRegisterName(regSrc2));
+    }
+}
